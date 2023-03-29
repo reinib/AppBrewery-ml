cee41ca86066f5f6318cc235ce86d075c3d41c36
Brent Reininger <brent.reininger89@gmail.com>
2023-03-29 16:46:06 -0700

    Python exercises

--- MLProjects/life.py
+++ MLProjects/life.py
@@ -1,1 +1,9 @@
+import math
+
 theAnswer = 42
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+
+def square_root(value):
+    return math.sqrt(value)
